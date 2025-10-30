17701a68d63423be7d0fab5f74908a3e9894fadd
Sameer-Sharma-SAM <www.sameersharma12@gmail.com>
2025-10-30 15:35:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
+getSumBtn.textContent = "Get Total Price";
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const prices = document.querySelectorAll(".price");
+  let total = 0;
+  prices.forEach(p => total += parseFloat(p.textContent));
+
+  const totalRow = document.createElement("tr");
+  const totalCell = document.createElement("td");
+  totalCell.colSpan = 2;
+  totalCell.textContent = `Total Price: ₹${total}`;
+  totalRow.appendChild(totalCell);
+
+  document.querySelector("table").appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
